c3fe25f4e1997e9b4cefa559a64df265233ef660
BYB_PRO <jm.castillo.ag@gmail.com>
2026-03-20 15:31:31 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCvHPNgceh6YlD1DJKPpMazeOuaUX2K_lE",
@@ -14,8 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const storage = getStorage(app);
 const dbRef = ref(db, 'taller_byb');
 const usersRef = ref(db, 'usuarios_byb');
 
-export { db, storage, dbRef, usersRef, set, onValue, sRef, uploadBytes, getDownloadURL };
+export { db, dbRef, usersRef, set, onValue };
